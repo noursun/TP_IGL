4a511af0b80bb7a82228d92a54c9d846984c4ccb
NourElHouda <fn_mehadjibia@esi.dz>
2017-10-19 13:02:40 +0200

    Je viens de créer les classes (OutilsString et OutilsString Test)

---
+++ src/Application1/OutilsString.java
@@ -0,0 +1,5 @@
+package Application1;
+
+public class OutilsString {
+
+}
---
+++ src/Application1/OutilsStringTest.java
@@ -0,0 +1,14 @@
+package Application1;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class OutilsStringTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
