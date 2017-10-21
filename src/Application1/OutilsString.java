6448746c519e652b66af2927ceac05e157e4baf1
mimene <fi_rahmoune@esi.dz>
2017-10-21 20:46:03 +0200

    j'ai ajouté les fonctions suivantes : fractionner une chaine, majuscule et minuscule, remplacer par la lettre suivante et leurs tests unitaires

--- src/Application1/OutilsString.java
+++ src/Application1/OutilsString.java
@@ -1,5 +1,56 @@
 package Application1;
 
+import java.util.ArrayList;
+
 public class OutilsString {
+	
+	public static String[] frac(char s,String chaine){
+
+        ArrayList<String> nouvs = new ArrayList<String>();
+		char r;
+		String temp="";
+		int i=0;
+		while(i<chaine.length()){
+			r=chaine.charAt(i);
+			if(s==r){
+				nouvs.add(temp);
+				temp="";
+				}		
+			i++;
+			temp= temp+ Character.toString(r) ;
+		}
+		return nouvs.toArray(new String[0]);
+		
+	}
+	
+	public static String MajMin(String chaine){
+		String nouv = "";
+		int i=1;
+		nouv=nouv+Character.toUpperCase(chaine.charAt(0));
+
+		while(i<chaine.length()){
+			nouv=nouv+Character.toLowerCase(chaine.charAt(i));
+			i++;
+		}
+		
+		return nouv;
+	}
+
+	public static String SuivLettre(String chaine){
+		int i=1;
+		String suiv="";
+			
+		while(i<chaine.length()){
+			suiv=suiv+chaine.charAt(i);
+			i++;
+		}
+			
+		return suiv;
+	}
+
+	
 
 }
+
+
+
--- src/Application1/OutilsStringTest.java
+++ src/Application1/OutilsStringTest.java
@@ -2,13 +2,49 @@ package Application1;
 
 import static org.junit.Assert.*;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class OutilsStringTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testFracVrai() {
+		String chaine="imeimen";
+		char  s='i';
+		String[] essai=OutilsString.frac(s,chaine);
+		Boolean bool=true;
+		if (essai[0]!="ime") bool=false;
+		if (essai[1]!="imen") bool=false;
+		if(bool==true)fail("Resulat vrai");
 	}
+
+	@Test
+	public void testFracFaux() {
+		String chaine="imeimen";
+		char  s='i';
+		String[] essai=OutilsString.frac(s,chaine);
+		Boolean bool=true;
+		if (essai[0]=="im") bool=false;
+		if (essai[1]=="imm") bool=false;
+		
+		if(bool==false)fail("Resulat faux");
+	}
+
+	@Test
+	void testMajMinEqualVrai() {
+		String s="j'ai Le  Droit99", att="J'ai le  droit99",res="";
+		res=OutilsString.MajMin(s);
+		Assert.assertEquals(res, att);
+		
+	}
+
+	
+	@Test
+	void testSuivLettre() {
+		String s="aaabc", att="aabc",res="";
+		res=OutilsString.SuivLettre(s);
+		Assert.assertEquals(res, att);
+	}
+	
 
 }
